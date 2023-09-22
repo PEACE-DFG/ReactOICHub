33165f2adcd8f4a837bb2e36a71e2cd3348bce8e
PEACE-DFG <awofesobipeace@gmail.com>
2023-09-22 15:09:59 +0100

    first react js class

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  // Javascript code
+  return (
+    <>
+   <h1>First REACT JS </h1>
+   {
+    console.log(2+3 )
+   }
+    </>
+  )
+}
+
+export default App
